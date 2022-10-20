9713591b5b2a5a672c96bf49aa758c9c1790fab9
jacquestredoux <jacques.trdx@gmail.com>
2022-10-20 09:34:21 +0200

    Initial

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -9,6 +9,27 @@ class Person extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'institution_id',
+        'id',
+        'user_id',
+        'profile_picture',
+        'first_name',
+        'surname',
+        'maiden_name',
+        'nickname',
+        'gender',
+        'race',
+        'marital_status',
+        'weight',
+        'length',
+        'birth_place',
+        'date_of_birth',
+        'age',
+        'nationality',
+        'language_spoken',
+        'religion',
+        'uid',];
     public function institutions()
     {
         return $this->belongsToMany(Institution::class, 'institution_people');
