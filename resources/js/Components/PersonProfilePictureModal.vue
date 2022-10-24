9021dfc984dcdeba864d7d2cdb110567f16f5514
jacquestredoux <jacques.trdx@gmail.com>
2022-10-24 11:30:11 +0200

    Inisitaal

--- resources/js/Components/PersonProfilePictureModal.vue
+++ resources/js/Components/PersonProfilePictureModal.vue
@@ -1,7 +1,7 @@
 
 <template>
     <div>
-        <v-tailwind-modal v-model="show" @confirm="confirm" @cancel="cancel">
+        <v-tailwind-modal v-if="show" @confirm="confirm" @cancel="cancel">
             <template v-slot:title>Hello, vue-final-modal</template>
             <p>Vue Final Modal is a renderless, stackable, detachable and lightweight modal component.</p>
         </v-tailwind-modal>
