bd4f7a1413bb89bea73b2a4f9d574340e90f2943
jacquestredoux <jacques.trdx@gmail.com>
2022-10-24 11:58:25 +0200

    Inisitaal

--- resources/js/Components/PersonProfilePictureModal.vue
+++ resources/js/Components/PersonProfilePictureModal.vue
@@ -10,55 +10,18 @@ export default {
 </script>
 
 <template>
-    <transition name="modal-fade">
-        <div class="modal-backdrop">
-            <div class="modal"
-                 role="dialog"
-                 aria-labelledby="modalTitle"
-                 aria-describedby="modalDescription"
-            >
-                <header
-                    class="modal-header"
-                    id="modalTitle"
-                >
-                    <slot name="header">
-                        This is the default tile!
-                    </slot>
-                    <button
-                        type="button"
-                        class="btn-close"
-                        @click="close"
-                        aria-label="Close modal"
-                    >
-                        x
-                    </button>
-                </header>
+    <div class="modal-backdrop">
+        <div class="modal">
+            <slot name="header">
+            </slot>
 
-                <section
-                    class="modal-body"
-                    id="modalDescription"
-                >
-                    <slot name="body">
-                        This is the default body!
-                    </slot>
-                </section>
+            <slot name="body">
+            </slot>
 
-                <footer class="modal-footer">
-                    <slot name="footer">
-                        This is the default footer!
-                    </slot>
-                    <button
-                        type="button"
-                        class="btn-green"
-                        @click="close"
-                        aria-label="Close modal"
-                    >
-                        Close me!
-                    </button>
-                </footer>
-            </div>
+            <slot name="footer">
+            </slot>
         </div>
-    </transition>
+    </div>
 </template>
 
 <style>
