d54cb0399d397b7ccd82c67f6f39c7c983540355
jacquestredoux <jacques.trdx@gmail.com>
2022-10-24 11:34:22 +0200

    Inisitaal

--- resources/js/Components/PersonProfilePictureModal.vue
+++ resources/js/Components/PersonProfilePictureModal.vue
@@ -4,7 +4,7 @@
         <vue-final-modal v-model="show">
             Modal Content Here
         </vue-final-modal>
-        <button @click="showModal = true">Open Modal</button>
+        <button @click="show = true">Open Modal</button>
     </div>
 </template>
 
