082028e3f346a8a0809b2fe66183f1322e986341
jacquestredoux <jacques.trdx@gmail.com>
2022-10-24 10:50:43 +0200

    Inisitaal

--- resources/js/Pages/Person/Edit.vue
+++ resources/js/Pages/Person/Edit.vue
@@ -36,7 +36,6 @@ export default {
             nationality: props.person.nationality,
             language_spoken: props.person.language_spoken,
             religion: props.person.religion,
-            file_upload: null,
         });
         return {
             form
@@ -54,11 +53,7 @@ export default {
             window.location.href = '/admin';
         },
         submitPersonForm(){
-            console.log(this.form);
-            this.form.post(`/person/${this.form.person.id}`, {
-                _method: 'put',
-                file_upload: this.form.file_upload,
-            })
+            this.form.put(route("person.update", this.form.person.id));
         }
     }
 }
@@ -210,30 +205,6 @@ export default {
                                 </div>
                             </div>
                         </div>
-
-                        <div class="mt-5 md:col-span-2 md:mt-0">
-                                    <div class="shadow sm:overflow-hidden sm:rounded-md">
-                                        <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
-                                            <div>
-                                                <label class="block text-sm font-medium text-gray-700"> Photo</label>
-                                                <div class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                                                    <div class="space-y-1 text-center">
-                                                        <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                                                            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                                        </svg>
-                                                        <div class="flex text-sm text-gray-600">
-                                                            <label for="file_upload" class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
-                                                                <span>Upload a file</span>
-                                                            </label>
-                                                            <input id="file_upload" type="file" @input="this.form.file_upload = $event.target.files[0]" />
-                                                        </div>
-                                                        <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                            </div>
 
                             <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
                                 <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
