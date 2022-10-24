6d873f392d7cb8a30ad3e9531f7a9abb09a8693c
jacquestredoux <jacques.trdx@gmail.com>
2022-10-24 12:59:24 +0200

    fsd

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -143,11 +143,7 @@ class PersonController extends Controller
         $person = Person::find($request->person_id);
         if($request->hasFile('profile_picture')){
             $fileName = $person->id.'_profile_pic.'.$request->file('profile_picture')->getClientOriginalExtension();
-            if(!directoryExists(public_path('person'))){
-                mkdir(public_path('person'));
-            }
             $request->file('profile_picture')->move(public_path('person'), $fileName);
-
         }
         $person->profile_picture = $fileName;
         $person->save();
