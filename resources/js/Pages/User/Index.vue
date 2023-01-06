6c0dcd69b3c905e7f3df6969cf068f43ac746d5c
jacquestredoux <jacques.trdx@gmail.com>
2023-01-06 09:40:40 +0200

    Removing User->Institution Link

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -53,7 +53,7 @@ export default {
                                         <thead class="bg-gray-50">
                                         <tr>
                                             <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
-                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Institute</th>
+                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Departments</th>
                                             <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
                                             <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
                                             <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
