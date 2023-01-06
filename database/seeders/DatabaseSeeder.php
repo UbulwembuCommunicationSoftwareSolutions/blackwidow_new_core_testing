1ea1ee2ec4ef10d89c6da2de0ca93cd08853bfc4
jacquestredoux <jacques.trdx@gmail.com>
2023-01-06 09:31:08 +0200

    Removing User->Institution Link

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -74,9 +74,9 @@ class DatabaseSeeder extends Seeder
             'id' => '2',
             'description' => 'Second institute',
         ]);
-        DB::table('institution_user')->insert([
+        DB::table('department_user')->insert([
             'id' => '1',
-            'institution_id' => '1',
+            'department_id' => '1',
             'user_id' => '1',
         ]);
 
