be55457a6b37d7f665ad4d1f767d7d07c3ce401e
jacquestredoux <jacques.trdx@gmail.com>
2023-01-19 12:22:44 +0200

    Added incidents Model

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -140,12 +140,6 @@ const showingNavigationDropdown = ref(false);
                 </div>
             </div>
         </nav>
-
-        <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-            </div>
-        </header>
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
                 <!-- Replace with your content -->
