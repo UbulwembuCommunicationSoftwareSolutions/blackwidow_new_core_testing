d3d94c776d526a7217521cee37b61588e20e5d1c
jacquestredoux <jacques.trdx@gmail.com>
2023-01-24 14:30:13 +0200

    Added incidents Model

--- resources/js/Components/VueDataTable.vue
+++ resources/js/Components/VueDataTable.vue
@@ -6,18 +6,38 @@
             <option value="active">Active</option>
             <option value="inactive">Inactive</option>
         </select>
-        <table>
-            <thead>
-            <tr>
-                <th v-for="(column) in this.columns" @click="sortBy(column)">{{ column }}</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="item in filteredItems" :key="item.id">
-                <td v-for="(value, key) in item" :key="key">{{value}}</td>
-            </tr>
-            </tbody>
-        </table>
+            <div class="px-4 sm:px-6 lg:px-8">
+                <div class="sm:flex sm:items-center">
+                    <div class="sm:flex-auto">
+                        <h1 class="text-xl font-semibold text-gray-900">Incidents</h1>
+                        <p class="mt-2 text-sm text-gray-700"></p>
+                    </div>
+                    <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                        <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
+                            Add Incident</button>
+                    </div>
+                </div>
+                <div class="mt-8 flex flex-col">
+                    <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                <table class="min-w-full divide-y divide-gray-300">
+                                    <thead class="bg-gray-50">
+                                    <tr>
+                                        <th v-for="(column) in this.columns" @click="sortBy(column)">{{ column }}</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="divide-y divide-gray-200 bg-white">
+                                    <tr v-for="item in filteredItems" :key="item.id">
+                                        <td v-for="(value, key) in item" :key="key">{{value}}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
 </template>
 
