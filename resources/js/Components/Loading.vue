1f753ac8f193b31852fbc28458150ce9dae201b6
jacquestredoux <jacques.trdx@gmail.com>
2023-01-24 15:24:27 +0200

    Added incidents Model

--- resources/js/Components/Loading.vue
+++ resources/js/Components/Loading.vue
@@ -1,3 +1,17 @@
+<template>
+    <div class="loading">
+        <div class="loading-icon"></div>
+    </div>
+</template>
+
+<style>
+.loading {
+}
+
+.loading-icon {
+// styles for loading icon
+}
+</style>
 
 <script>
 export default {
