a142246c19c4fb2294d7be97bfc19869458a273f
jacquestredoux <jacques.trdx@gmail.com>
2023-01-24 15:22:14 +0200

    Added incidents Model

--- resources/js/Components/Loading.vue
+++ resources/js/Components/Loading.vue
@@ -1,20 +1,3 @@
-<template>
-    <div class="loading">
-        <div class="loading-icon"></div>
-    </div>
-</template>
-
-<style>
-.loading {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-}
-
-.loading-icon {
-// styles for loading icon
-}
-</style>
 
 <script>
 export default {
