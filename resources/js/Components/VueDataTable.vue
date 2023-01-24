a338eb096f29711c22677e6f947a4cce4107b185
jacquestredoux <jacques.trdx@gmail.com>
2023-01-24 15:27:32 +0200

    Added incidents Model

--- resources/js/Components/VueDataTable.vue
+++ resources/js/Components/VueDataTable.vue
@@ -56,7 +56,7 @@ export default {
             selectedFilter: 'all',
             sortBy: null,
             sortDesc: false,
-            loading : false
+            loading : true
         }
     },
     setup(props){
