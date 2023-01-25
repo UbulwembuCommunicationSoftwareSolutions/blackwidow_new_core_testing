b84c358b84cf9452d6c87f4ab8ab70cae01a3bc7
jacquestredoux <jacques.trdx@gmail.com>
2023-01-25 10:56:10 +0200

    zx

--- resources/js/Pages/Incident/Index.vue
+++ resources/js/Pages/Incident/Index.vue
@@ -51,8 +51,7 @@ export default {
                                 <p class="mt-2 text-sm text-gray-700">A list of all the incidents in your system including their name, title, email and role.</p>
                             </div>
                             <div>
-                                <p>path: {{ $props.$route.path }}</p>
-                                <p>query: {{ JSON.stringify($props.$route.query) }}</p>
+                                <p>path: {{ $route.url }}</p>
                             </div>
                             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                                 <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add incident</button>
