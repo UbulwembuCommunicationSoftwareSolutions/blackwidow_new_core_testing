4931e24fdbb614893f4cbcb2cc5dccfcc5ed30e9
jacquestredoux <jacques.trdx@gmail.com>
2023-01-25 13:32:56 +0200

    zx

--- app/Models/Incident.php
+++ app/Models/Incident.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Incident extends Model
 {
     use HasFactory;
-
+    protected $dateFormat = 'Y-m-d H:i:s';
     public function user(){
         return $this->belongsTo(User::class);
     }
