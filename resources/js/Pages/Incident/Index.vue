cbb59795c419d60f72dae34adaa91be810289171
jacquestredoux <jacques.trdx@gmail.com>
2023-01-25 12:57:28 +0200

    zx

--- resources/js/Pages/Incident/Index.vue
+++ resources/js/Pages/Incident/Index.vue
@@ -65,8 +65,9 @@ export default {
                                 <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                                     <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                                         <div>
+                                            <input class="mt-4 mb-4 ml-4" v-model="search" @input="this.searchTable" placeholder="Search" />
+
                                             <Loading v-if="this.loading"/>
-                                            <input v-model="search" @input="this.searchTable" placeholder="Search" />
                                             <div class="px-4 sm:px-6 lg:px-8">
                                                 <div class="mt-8 flex flex-col">
                                                     <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
