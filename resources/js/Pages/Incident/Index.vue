d4f0ca0ed4955ec3fa03a0e865e0b3ce6ec3df2f
jacquestredoux <jacques.trdx@gmail.com>
2023-01-25 11:30:50 +0200

    zx

--- resources/js/Pages/Incident/Index.vue
+++ resources/js/Pages/Incident/Index.vue
@@ -25,8 +25,6 @@ export default {
     methods: {
         searchTable(){
             Inertia.reload({
-                preserveState: true,
-                only: ['incidents'],
                 query: {
                     search: this.search
                 }
