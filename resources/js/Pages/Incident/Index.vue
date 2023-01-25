8a515d2c79c031093bc8dd4e4cd463ed45f3f11b
jacquestredoux <jacques.trdx@gmail.com>
2023-01-25 10:50:08 +0200

    zx

--- resources/js/Pages/Incident/Index.vue
+++ resources/js/Pages/Incident/Index.vue
@@ -40,10 +40,6 @@ export default {
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Incidents
             </h2>
-            <div>
-                <p>path: {{ props.$route.path }}</p>
-                <p>query: {{ JSON.stringify(props.$route.query) }}</p>
-            </div>
         </template>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 sm:py-6 lg:py-8">
@@ -53,6 +49,10 @@ export default {
                             <div class="sm:flex-auto">
                                 <h1 class="text-xl font-semibold text-gray-900">Incidents</h1>
                                 <p class="mt-2 text-sm text-gray-700">A list of all the incidents in your system including their name, title, email and role.</p>
+                            </div>
+                            <div>
+                                <p>path: {{ props.$route.path }}</p>
+                                <p>query: {{ JSON.stringify(props.$route.query) }}</p>
                             </div>
                             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                                 <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add incident</button>
