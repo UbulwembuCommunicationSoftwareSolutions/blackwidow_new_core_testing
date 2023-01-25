9d0d55274627aa95c603c161c114bf1b45d7f79a
jacquestredoux <jacques.trdx@gmail.com>
2023-01-25 13:42:53 +0200

    zx

--- resources/js/Pages/Incident/Index.vue
+++ resources/js/Pages/Incident/Index.vue
@@ -81,21 +81,30 @@ export default {
                                                                 <table class="min-w-full divide-y divide-gray-300">
                                                                     <thead class="bg-gray-50">
                                                                     <tr>
-                                                                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Id</th>
-                                                                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"> Description</th>
+                                                                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Id</th>
+                                                                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Description</th>
                                                                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">User</th>
-                                                                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Created</th>
+                                                                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Date</th>
+                                                                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                                                            <span class="sr-only">Edit</span>
+                                                                        </th>
                                                                     </tr>
                                                                     </thead>
-                                                                    <tbody class="divide-y divide-gray-200 bg-white">
+                                                                    <tbody class="bg-white">
                                                                     <tr v-for="(incident,incident_id) in incidents.data" :key="incident.id" :class="incident_id % 2 === 0 ? undefined : 'bg-gray-50'">
                                                                         <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ incident.id }}</td>
-                                                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ incident.description }}</td>
-                                                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"></td>
-                                                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ incident.created_at }}</td>
+                                                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ incident.description }}</td>
+                                                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"></td>
+                                                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ incident.created_at }}</td>
+                                                                        <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                                            <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                                                                            >Edit<span class="sr-only"> Edit </span></a
+                                                                            >
+                                                                        </td>
                                                                     </tr>
                                                                     </tbody>
                                                                 </table>
+
                                                             </div>
                                                             <Pagination class="mt-6" :links="incidents.links" />
                                                         </div>
