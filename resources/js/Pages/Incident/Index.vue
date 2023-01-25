c02cd0bbc3b0118cd75b166535d586b3ef7512d0
jacquestredoux <jacques.trdx@gmail.com>
2023-01-25 10:58:40 +0200

    zx

--- resources/js/Pages/Incident/Index.vue
+++ resources/js/Pages/Incident/Index.vue
@@ -24,11 +24,11 @@ export default {
     },
     methods: {
         searchTable(){
-            Inertia.get(props.$route.path);
+            Inertia.get();
         }
     },
     mounted(){
-        console.log(this.incidents);
+        console.log($page.props.ziggy);
     }
 }
 </script>
@@ -51,7 +51,7 @@ export default {
                                 <p class="mt-2 text-sm text-gray-700">A list of all the incidents in your system including their name, title, email and role.</p>
                             </div>
                             <div>
-                                <p>path: {{ $route.url }}</p>
+
                             </div>
                             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                                 <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add incident</button>
