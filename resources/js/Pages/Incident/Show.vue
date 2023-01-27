67f351b863aa2c9dc0e8cbd7c7a0c69edb77f638
jacquestredoux <jacques.trdx@gmail.com>
2023-01-27 08:49:34 +0200

    zx

--- resources/js/Pages/Incident/Show.vue
+++ resources/js/Pages/Incident/Show.vue
@@ -20,7 +20,14 @@
                             </div>
                             <div class="hidden sm:block">
                                 <nav class="isolate flex divide-x divide-gray-200 rounded-lg shadow" aria-label="Tabs">
-                                    <a v-for="(tab, tabIdx) in tabs" :key="tab.name" :href="tab.href" :class="[tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700', tabIdx === 0 ? 'rounded-l-lg' : '', tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '', 'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10']" :aria-current="tab.current ? 'page' : undefined">
+                                    <a v-for="(tab, tabIdx) in tabs" :key="tab.name"
+                                       onclick="this.changeTab(tab.id)"
+                                       :class="[tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+                                        tabIdx === 0 ? 'rounded-l-lg' : '',
+                                        tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
+                                         'group relative min-w-0 flex-1 overflow-hidden' +
+                                          ' bg-white py-4 px-4 text-sm font-medium' +
+                                           ' text-center hover:bg-gray-50 focus:z-10']" :aria-current="tab.current ? 'page' : undefined">
                                         <span>{{ tab.name }}</span>
                                         <span aria-hidden="true" :class="[tab.current ? 'bg-indigo-500' : 'bg-transparent', 'absolute inset-x-0 bottom-0 h-0.5']" />
                                     </a>
@@ -48,15 +55,25 @@ export default {
     },
     setup(props){
         const tabs = [
-            { name: 'Applied', href: '#', count: '52', current: false },
-            { name: 'Phone Screening', href: '#', count: '6', current: false },
-            { name: 'Interview', href: '#', count: '4', current: true },
-            { name: 'Offer', href: '#', current: false },
-            { name: 'Disqualified', href: '#', current: false },
+            { name: 'Applied', href: '#', id: '1', current: false },
+            { name: 'Phone Screening', href: '#', id: '2', current: false },
+            { name: 'Interview', href: '#', id: '3', current: true },
+            { name: 'Offer', href: '#', id: '4',current: false },
+            { name: 'Disqualified', id :'5', href: '#', current: false },
         ]
         let incident = props.incident
         return {
             tabs,incident
+        }
+    },
+    methods: {
+        changeTab(id){
+            this.tabs.forEach((tab) => {
+                tab.current = false;
+                if(tab.id===id){
+                    tab.current = true;
+                }
+            });
         }
     }
 }
