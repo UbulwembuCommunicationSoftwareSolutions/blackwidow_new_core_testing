c0cc0e58c195215fedc72f1d2eb620351e66084e
jacquestredoux <jacques.trdx@gmail.com>
2023-01-27 08:34:08 +0200

    zx

--- resources/js/Pages/Incident/Index.vue
+++ resources/js/Pages/Incident/Index.vue
@@ -55,9 +55,6 @@ export default {
                                 <h1 class="text-xl font-semibold text-gray-900">Incidents</h1>
                                 <p class="mt-2 text-sm text-gray-700">A list of all the incidents in your system including their name, title, email and role.</p>
                             </div>
-                            <div>
-
-                            </div>
                             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                                 <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add incident</button>
                             </div>
