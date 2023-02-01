27832296d37772363fa263f68de0bd9584cedc4a
jacquestrdx123 <jacques.trdx@gmail.com>
2023-02-01 18:02:15 +0200

    zx

--- resources/js/Pages/Incident/Page2.vue
+++ resources/js/Pages/Incident/Page2.vue
@@ -23,7 +23,7 @@
                                             </tr>
                                             </thead>
                                             <tbody class="bg-white">
-                                            <tr v-for="(person,person.id) in incident.people" :key="person.id" :class="person.id % 2 === 0 ? undefined : 'bg-gray-50'">
+                                            <tr v-for="(person,person_id) in incident.people" :key="person.id" :class="person.id % 2 === 0 ? undefined : 'bg-gray-50'">
                                                 <td class="whitespace-nowrap text-sm font-medium text-blue-600 sm:pl-3">
                                                     <a class="mr-1 mb-1 text-sm leading-4  rounded hover:bg-white focus:border-indigo-500 focus:text-blue-500"
                                                        :href="'/person/'+person.id+'/edit'">
