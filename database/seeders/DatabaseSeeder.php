a037acf675c4a1c1407c0dd430046565f0041f72
jacquestrdx123 <jacques.trdx@gmail.com>
2023-02-01 14:40:13 +0200

    zx

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -79,13 +79,23 @@ class DatabaseSeeder extends Seeder
             'description' => 'Second institute',
         ]);
         DB::table('department_user')->insert([
-            'id' => '1',
             'department_id' => '1',
             'user_id' => '1',
         ]);
         DB::table('department_user')->insert([
-            'id' => '2',
             'department_id' => '2',
+            'user_id' => '1',
+        ]);
+        DB::table('department_user')->insert([
+            'department_id' => '3',
+            'user_id' => '1',
+        ]);
+        DB::table('department_user')->insert([
+            'department_id' => '4',
+            'user_id' => '1',
+        ]);
+        DB::table('department_user')->insert([
+            'department_id' => '5',
             'user_id' => '1',
         ]);
         $genders = array();
