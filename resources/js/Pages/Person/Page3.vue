9b306fbf3023ec60f03c4f8b471255de125d7251
jacquestrdx123 <jacques.trdx@gmail.com>
2023-02-08 13:22:55 +0200

    zx

--- resources/js/Pages/Person/Page3.vue
+++ resources/js/Pages/Person/Page3.vue
@@ -22,7 +22,7 @@
                                             <div class="mt-8 flex flex-col">
                                                 <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                                                     <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                                                        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                                        <div class="overflow-hidden shadow ring-1 mb-4 ring-black ring-opacity-5 md:rounded-lg">
                                                             <table class="min-w-full mb-4 divide-y divide-gray-300">
                                                                 <thead class="bg-gray-50">
                                                                 <tr>
