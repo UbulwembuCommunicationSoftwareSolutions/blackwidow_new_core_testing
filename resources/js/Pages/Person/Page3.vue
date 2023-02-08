1118daeaf512458cf6ba69f6dc3d36213bff491c
jacquestrdx123 <jacques.trdx@gmail.com>
2023-02-08 14:50:50 +0200

    zx

--- resources/js/Pages/Person/Page3.vue
+++ resources/js/Pages/Person/Page3.vue
@@ -162,10 +162,15 @@ export default {
 
         layoutNodes[`person${person.id}`] = { x: nodeArray[0].x, y: nodeArray[0].y };
 
-        this.incidents.people.forEach((person) => {
-            nodes[`person${person.id}`] = { name: 'Person: '+person.id };
-            edges[`person${person.id}`] = { source: `incident${person.incident.id}` , target: `person${person.id}` }
-            layoutNodes[`person${person.id}`] = { x: layoutNodes[`incident${person.incident.id}`].x*1.5, y: layoutNodes[`incident${person.incident.id}`].y*1.5 };
+        this.incidents.forEach((incident) => {
+            this.incident.people.forEach((person) => {
+                nodes[`person${person.id}`] = {name: 'Person: ' + person.id};
+                edges[`person${person.id}`] = {source: `incident${incident.id}`, target: `person${person.id}`}
+                layoutNodes[`person${person.id}`] = {
+                    x: layoutNodes[`incident${incident.id}`].x * 1.5,
+                    y: layoutNodes[`incident${incident.id}`].y * 1.5
+                };
+            });
         });
 
         let layouts = {
