0042a057e28e93a44c235c22c5e4a6ba8bdb9cca
jacquestrdx123 <jacques.trdx@gmail.com>
2023-02-08 19:24:36 +0200

    zx

--- resources/js/Components/NetworkDiagram.vue
+++ resources/js/Components/NetworkDiagram.vue
@@ -1,9 +1,9 @@
-<script>
+<script lang="ts">
 import { reactive, ref, watch } from "vue"
 import * as vNG from "v-network-graph"
-import {
-    ForceLayout,
-} from "v-network-graph/lib/force-layout"
+import { ForceLayout } from "v-network-graph/lib/force-layout"
+import { ForceNodeDatum } from "v-network-graph/lib/force-layout"
+import { ForceEdgeDatum } from "v-network-graph/lib/force-layout"
 
 export default {
     props: ['nodes','edges'],
@@ -19,16 +19,16 @@ export default {
                         positionFixedByClickWithAltKey: true,
                         // * The following are the default parameters for the simulation.
                         // * You can customize it by uncommenting below.
-                        // createSimulation: (d3, nodes, edges) => {
-                        //   const forceLink = d3.forceLink<ForceNodeDatum, ForceEdgeDatum>(edges).id(d => d.id)
-                        //   return d3
-                        //     .forceSimulation(nodes)
-                        //     .force("edge", forceLink.distance(100))
-                        //     .force("charge", d3.forceManyBody())
-                        //     .force("collide", d3.forceCollide(50).strength(0.2))
-                        //     .force("center", d3.forceCenter().strength(0.05))
-                        //     .alphaMin(0.001)
-                        // }
+                        createSimulation: (d3, nodes, edges) => {
+                          const forceLink = d3.forceLink<ForceNodeDatum, ForceEdgeDatum>(edges).id(d => d.id)
+                          return d3
+                            .forceSimulation(nodes)
+                            .force("edge", forceLink.distance(50))
+                            .force("charge", d3.forceManyBody())
+                            .force("collide", d3.forceCollide(50).strength(0.2))
+                            .force("center", d3.forceCenter().strength(0.05))
+                            .alphaMin(0.001)
+                        }
                     }),
                 },
                 node: {
@@ -40,6 +40,7 @@ export default {
                         color: "#000000",
                         margin: 4,
                         direction: "south",
+                        directionAutoAdjustment: true,
                         text: "name",
                     },
                 }
