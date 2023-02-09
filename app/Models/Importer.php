9805982613d8485ba738a801316ce7691107c722
jacquestrdx123 <jacques.trdx@gmail.com>
2023-02-09 08:41:25 +0200

    zx

--- app/Models/Importer.php
+++ app/Models/Importer.php
@@ -10,11 +10,12 @@ class Importer extends Model
 
     public static function Import($url){
         $token = Importer::Login($url);
+        dd($token);
     }
 
     public static function Login($url){
         $curl = curl_init();
-
+        $url .= '/api/authorize';
         curl_setopt_array($curl, array(
             CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
@@ -32,5 +33,49 @@ class Importer extends Model
         curl_close($curl);
         return $response;
     }
+
+    public static function getDepartments($url){
+
+        $curl = curl_init();
+        $url .= '/api/cases/departments';
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
+    }
+
+    public static function getUsers($url){
+
+        $curl = curl_init();
+        $url .= '/api/cases/admin_users';
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
+    }
+
 
 }
