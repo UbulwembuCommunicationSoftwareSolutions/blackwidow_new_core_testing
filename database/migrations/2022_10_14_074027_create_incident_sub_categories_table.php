6c0bff473128e9b883dfb3b1f9c27cd8b7843e10
jacquestrdx123 <jacques.trdx@gmail.com>
2023-02-09 07:28:28 +0200

    zx

---
+++ database/migrations/2022_10_14_074027_create_incident_sub_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incident_sub_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('description')->nullable();
+            $table->integer('incident_category_id')->nullable();
+            $table->index(['description'])->limit(50);
+            $table->index(['incident_category_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments');
+    }
+};
