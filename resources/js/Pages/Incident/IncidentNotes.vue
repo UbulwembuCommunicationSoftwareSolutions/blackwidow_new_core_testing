18791df5788aa1faac5754ee29f5879d3c5d5eae
jacquestrdx123 <jacques.trdx@gmail.com>
2023-02-15 10:22:52 +0200

    zx

--- resources/js/Pages/Incident/IncidentNotes.vue
+++ resources/js/Pages/Incident/IncidentNotes.vue
@@ -22,10 +22,8 @@
                                             </thead>
                                             <tbody class="bg-white">
                                             <tr v-for="(incident_note,incident_note_id) in incident.incident_notes" :key="incident_note_id" :class="incident_note_id % 2 === 0 ? undefined : 'bg-gray-50'">
-                                                <td class="break-words text-sm font-medium text-blue-600 sm:pl-3">
-                                                    <div  v-html="sanitize(incident_note.note)" class="mr-1 mb-1 text-sm leading-4  rounded hover:bg-white focus:border-indigo-500 focus:text-blue-500">
+                                                <td  v-html="sanitize(incident_note.note)" class="break-words text-sm font-medium text-blue-600 sm:pl-3">
 
-                                                    </div>
                                                 </td>
                                                 <td class="whitespace-nowrap  text-sm font-medium text-blue-600 sm:pl-3">
                                                     <p class="mr-1 mb-1 text-sm leading-4  rounded hover:bg-white focus:border-indigo-500 focus:text-blue-500">
