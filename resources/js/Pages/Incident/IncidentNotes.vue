2e6b84a62b0c74b0a84f367541e85d7dff29d881
jacquestrdx123 <jacques.trdx@gmail.com>
2023-02-15 10:12:42 +0200

    zx

--- resources/js/Pages/Incident/IncidentNotes.vue
+++ resources/js/Pages/Incident/IncidentNotes.vue
@@ -3,7 +3,7 @@
         <div class="flex mt-4 py-6 w-full whitespace-nowrap px-6">
             <div class="overflow-hidden w-full bg-white shadow sm:rounded-lg">
                 <div class="px-4 py-5 sm:px-6">
-                    <h3 class="text-lg font-medium leading-6 text-gray-900">Incident Notes</h3>
+                    <h3 class="text-lg font-medium leading-6 text-gray-900">Incident Activity</h3>
                     <p class="mt-1 max-w-2xl text-sm text-gray-500"></p>
                 </div>
                 <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
@@ -15,40 +15,37 @@
                                         <table class="min-w-full divide-y divide-gray-300">
                                             <thead class="bg-gray-50">
                                             <tr>
-                                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Id</th>
-                                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">First Name</th>
-                                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Surname</th>
-                                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Institutions</th>
+                                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Note</th>
+                                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">User</th>
+                                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Message</th>
+                                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Description</th>
                                                 <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Date</th>
                                             </tr>
                                             </thead>
                                             <tbody class="bg-white">
-                                            <tr v-for="(note,note_id) in incident.incident_notes" :key="note_id" :class="note_id % 2 === 0 ? undefined : 'bg-gray-50'">
+                                            <tr v-for="(incident_note,incident_note_id) in incident.incident_notes" :key="incident_note_id" :class="incident_note_id % 2 === 0 ? undefined : 'bg-gray-50'">
                                                 <td class="whitespace-nowrap text-sm font-medium text-blue-600 sm:pl-3">
-                                                    <a class="mr-1 mb-1 text-sm leading-4  rounded hover:bg-white focus:border-indigo-500 focus:text-blue-500"
-                                                       :href="'/person/'+person.id+'/edit'">
-                                                        {{person.id}}
-                                                    </a>
+                                                    <p class="mr-1 mb-1 text-sm leading-4  rounded hover:bg-white focus:border-indigo-500 focus:text-blue-500">
+                                                        {{incident_note.note}}
+                                                    </p>
                                                 </td>
                                                 <td class="whitespace-nowrap  text-sm font-medium text-blue-600 sm:pl-3">
-                                                    <a class="mr-1 mb-1 text-sm  rounded hover:bg-white focus:border-indigo-500 focus:text-blue-500"
-                                                       :href="'/person/'+person.id+'/edit'">
-                                                        {{person.first_name}}
-                                                    </a>
+                                                    <p class="mr-1 mb-1 text-sm leading-4  rounded hover:bg-white focus:border-indigo-500 focus:text-blue-500">
+                                                        {{incident_note.user.first_name + ' '+ incident_note.user.surname}}
+                                                    </p>
                                                 </td>
                                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-blue-600 sm:pl-3">
-                                                    <a class="mr-1 mb-1 text-sm leading-4  rounded hover:bg-white focus:border-indigo-500 focus:text-blue-500"
-                                                       :href="'/person/'+person.id+'/edit'">
-                                                        {{person.surname}}
-                                                    </a>
-                                                </td>
-                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                    <p v-for="institute in person.institutions">
-                                                        {{institute.description}}
+                                                    <p class="mr-1 mb-1 text-sm leading-4  rounded hover:bg-white focus:border-indigo-500 focus:text-blue-500">
+                                                        {{incident_note.message}}
                                                     </p>
                                                 </td>
                                                 <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                    {{ person.created_at }}
+                                                    <p class="mr-1 mb-1 text-sm leading-4  rounded hover:bg-white focus:border-indigo-500 focus:text-blue-500">
+                                                        {{incident_note.description}}
+                                                    </p>
+                                                </td>
+                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                    {{ incident_note.created_at }}
                                                 </td>
                                             </tr>
                                             </tbody>
@@ -75,11 +72,20 @@
 import {PaperClipIcon} from "@heroicons/vue/20/solid";
 
 export default {
-    name: "IncidentNotes",
+    name: "IncidentActivity",
     props : ['incident'],
     components: {
         PaperClipIcon
     },
+    setup(props){
+        const incident = props.incident;
+        return{
+            incident
+        }
+    },
+    mounted(){
+        console.log(this.incident.incident_notes)
+    }
 
 }
 </script>
