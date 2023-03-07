4d055c28740e8b061ba75b9e719c8752a36adfc3
jacquestrdx123 <jacques.trdx@gmail.com>
2023-03-07 09:19:04 +0200

    Replaced Description with Name

--- database/migrations/2022_10_13_070804_create_ethnic_groups_table.php.php
+++ database/migrations/2022_10_13_070804_create_ethnic_groups_table.php.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('institutions');
+        Schema::dropIfExists('ethnic_groups');
     }
 };
